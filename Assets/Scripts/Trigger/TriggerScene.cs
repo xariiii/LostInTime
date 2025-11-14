a13cc962011e14e188053bc71658bd2c4ed029a0
Kamila Wydra <oforgotten.dev@gmail.com>
2025-11-14 14:27:43 +0100

    scenes

--- Assets/Scripts/Trigger/TriggerScene.cs
+++ Assets/Scripts/Trigger/TriggerScene.cs
@@ -6,7 +6,7 @@ public class TriggerScene : MonoBehaviour
     public string spawnPointTag = "SpawnPoint"; // tag for identifying spawn point
 
     private string currentSceneName; // to track the current scene
-
+    public GameObject playerPrefab;
     private void Start()
     {
         currentSceneName = SceneManager.GetActiveScene().name;
@@ -24,29 +24,41 @@ public class TriggerScene : MonoBehaviour
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+{
+    if (scene.name == sceneToLoad)
     {
+        // Znajdź istniejącego gracza
+        GameObject player = GameObject.FindWithTag("Player");
 
-
-        if (scene.name == sceneToLoad)
+        // Jeśli istnieje, ustaw jego pozycję na spawn point
+        GameObject spawnpoint = GameObject.FindWithTag(spawnPointTag);
+        if (player != null && spawnpoint != null)
         {
-            GameObject spawnpoint = GameObject.FindWithTag(spawnPointTag);
-            if (spawnpoint != null)
-            {
-                GameObject player = GameObject.FindWithTag("Player");
-                if (player != null)
-                {
-                    player.transform.position = spawnpoint.transform.position;
-                }
-            }
-            // Unload previous scene
-            SceneManager.UnloadSceneAsync(currentSceneName);
-
-            // Update the current scene name and unregister event handler
-
-            currentSceneName = sceneToLoad;
-            SceneManager.sceneLoaded -= OnSceneLoaded;
+            player.transform.position = spawnpoint.transform.position;
         }
+
+        // Ustawienia kursora i czasu gry
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
+        Time.timeScale = 1f;
+
+        // Wznowienie kontrolera gracza
+        var controller = player?.GetComponent<Artemis.FPController>();
+        if (controller != null)
+        {
+            controller.ResumeController();
+        }
+
+        // Odładuj poprzednią scenę
+        SceneManager.UnloadSceneAsync(currentSceneName);
+
+        // Zaktualizuj aktualną scenę i odłącz event handler
+        currentSceneName = sceneToLoad;
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
+}
+
+
     
     
 
