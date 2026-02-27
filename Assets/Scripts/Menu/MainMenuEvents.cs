19ad55ef120746b217cfb624c438ada6ce48627c
Izabela Majcherczyk <xeclipse.dev@gmail.com>
2026-02-27 13:12:56 +0100

    fix: tp

--- Assets/Scripts/Menu/MainMenuEvents.cs
+++ Assets/Scripts/Menu/MainMenuEvents.cs
@@ -201,7 +201,7 @@ public class MainMenuEvents : MonoBehaviour
         HidePanel(SettingsVisual);
         HidePanel(PauseMenuPanel);
 
-        SceneManager.LoadScene("geography");
+        SceneManager.LoadScene("geographyMap");
     }
 
 
